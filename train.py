1fc981c279f27db84249f87be162161ccb96244e
maysam-asser <your-email@gmail.com>
2026-03-25 19:51:41 +0200

    returntes2

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Note: To "fail" the test, you can set max_iter=1 or a very bad learning rate.
 model = SGDClassifier(
     learning_rate='constant', 
-    eta0=0.01, 
+    eta0=100, 
   
 )
 model.fit(X_train, y_train)
